01e7a2c8d36bade0287c97321bb0f8e98e7156e9
furkanacikgoz23 <furkkanacikgoz23@gmail.com>
2018-09-17 16:11:57 +0300

    Vira Bismillah

---
+++ MvcMusicStore/Controllers/StoreController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MvcMusicStore.Models;
+
+namespace MvcMusicStore.Controllers
+{
+    public class StoreController : Controller
+    {
+        //MusicStoreEntities storeDB = new MusicStoreEntities();
+        MvcMusicStoreEntities storeDB = new MvcMusicStoreEntities();
+        // GET: Store
+        public ActionResult Index()
+        {
+          
+            var genres = storeDB.Genre.ToList();
+            return View(genres);
+        }
+
+        
+        
+        public ActionResult Browse(string genre)
+        {
+            // Retrieve Genre and its Associated Albums from database
+            var genreModel = storeDB.Genre.Include("Albums")
+            .Single(g => g.Name == genre);
+            return View(genreModel);
+        }
+
+
+        //GET: /Store /Details
+        public ActionResult Details(int id)
+        {
+            var album = storeDB.Album.Find(id);
+            return View(album);
+        }
+
+
+
+    }
+}
---
+++ MvcMusicStore/Controllers/StoreManagerController.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcMusicStore.Controllers
+{
+    public class StoreManagerController : Controller
+    {
+        // GET: StoreManager
+        public ActionResult Index()
+        {
+            return View();
+        }
+        //
+        // GET: /StoreManager/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        //
+        // POST: /StoreManager/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        //
+        // GET: /StoreManager/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+        //
+        // POST: /StoreManager/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+        //
+        // GET: /StoreManager/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+        //
+        // POST: /StoreManager/Delete/5
+
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
